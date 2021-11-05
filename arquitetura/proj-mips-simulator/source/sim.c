ebeb8b873bdf07b8e0caf870f312ae799aa7f052
viniciusMB <viniciusmartins@infojr.com.br>
2021-11-05 15:49:00 -0300

    novas funções mips

--- arquitetura/proj-mips-simulator/source/sim.c
+++ arquitetura/proj-mips-simulator/source/sim.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 #include "shell.h"
 
-void executeInstruction(uint32_t instruction) {
-    getUpcode(instruction);
-}
-
 void refreshPC(uint32_t add) {
     NEXT_STATE.PC = CURRENT_STATE.PC + add;
 }
 
-getUpcode(uint32_t instruction) {
+uint32_t getOpcode(uint32_t instruction) {
     return instruction >> 26;
 }
 
@@ -23,44 +19,76 @@ void syscall() {
     }
 }
 
-getShamt(uint32_t instruction) {
+uint32_t getShamt(uint32_t instruction) {
     instruction = instruction << 21;
     return instruction >> 27;
 }
 
-getFunct(uint32_t instruction) {
+uint32_t getFunct(uint32_t instruction) {
     instruction = instruction << 26;
     return instruction >> 26;
 }
 
-getAdress(uint32_t instruction) {
+uint32_t getAdress(uint32_t instruction) {
     instruction = instruction << 6;
     return instruction >> 6;
 }
 
-getRT(uint32_t instruction) {
+uint32_t getRT(uint32_t instruction) {
     instruction = instruction << 11;
     return instruction >> 27;
 }
 
-getRS(uint32_t instruction) {
+uint32_t getRS(uint32_t instruction) {
     instruction = instruction << 6;
     return instruction >> 27;
 }
 
-getRD(uint32_t instruction) {
+uint32_t getRD(uint32_t instruction) {
     instruction = instruction << 16;
     return instruction >> 27;
 }
 
-void add(uint32_t rs, uint32_t rt, uint32_t rd) {
+uint32_t getImmediate(uint32_t instruction) {
+    instruction = instruction << 16;
+    return instruction >> 16;
+}
+
+void addiu(uint32_t rs, uint32_t imm, uint32_t rd) {
+    NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rs] + CURRENT_STATE.REGS[imm];
+}
+
+void addu(uint32_t rs, uint32_t rt, uint32_t rd) {
     NEXT_STATE.REGS[rd] = CURRENT_STATE.REGS[rs] + CURRENT_STATE.REGS[rt];
 }
 
-void process_instruction()
-{
+void executeInstruction(uint32_t instruction) {
+    uint32_t opCode = getOpcode(instruction);
 
-    printf("Aqui na funcao process_instruction\n");
+    
+    if ( opCode == 0b1001) {             // addiu
+        uint32_t rs = getRS(instruction);
+        uint32_t rd = getRD(instruction);
+        uint32_t imm = getImmediate(instruction);
+        addiu(rs, imm, rd);
+        syscall();
+    }
+
+    if (opCode == 0b0000) {             // addu
+        uint32_t rs = getRS(instruction);
+        uint32_t rd = getRD(instruction);
+        uint32_t rt = getRT(instruction);
+        addu(rs, rt, rd);
+        syscall();
+    }
+
+    
+
+}
+
+void process_instruction() {
+    uint32_t instruction = mem_read_32(CURRENT_STATE.PC); 
+    executeInstruction(instruction);
     /* execute one instruction here. You should use CURRENT_STATE and modify
      * values in NEXT_STATE. You can call mem_read_32() and mem_write_32() to
      * access memory. */
